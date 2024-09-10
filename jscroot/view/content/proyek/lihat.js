453b225157ccefbd34c8cffa2365f6d7fd9e1b7f
Rolly Maulana Awangga <awangga@gmail.com>
2024-09-10 13:37:18 +0700

    documentButton

--- jscroot/view/content/proyek/lihat.js
+++ jscroot/view/content/proyek/lihat.js
@@ -96,6 +96,9 @@ function getResponseFunction(result) {
             <span class="full-text" style="display:none;">${project.description}</span>
           </td>
           <td class="has-text-centered">
+            <button class="button is-success documentButton" data-project-id="${project._id}">
+              <i class="bx bx-file"></i>
+            </button>
             <button class="button is-danger removeProjectButton" data-project-name="${project.name}">
               <i class="bx bx-trash"></i>          
             </button>
