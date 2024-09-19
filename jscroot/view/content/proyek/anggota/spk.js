8162fff26535176bd4b77d7363f690647fa98e38
Rolly Maulana Awangga <awangga@gmail.com>
2024-09-19 17:20:19 +0700

    getFileWithHeader

--- jscroot/view/content/proyek/anggota/spk.js
+++ jscroot/view/content/proyek/anggota/spk.js
@@ -92,11 +92,45 @@ export function SPKButtonListeners() {
     document.querySelectorAll(".downloadButton").forEach((button) => {
       button.addEventListener("click", async (event) => {
         const prjname = button.getAttribute("data-project-name");
-        getJSON(backend.project.downloadspk+btoa(prjname),'login',getCookie('login'),runafterDownloadDraft);
+        getFileWithHeader(backend.project.downloadspk+btoa(prjname),'login',getCookie('login'),runafterDownloadDraft);
       });
     });
   }
 
   function runafterDownloadDraft(response){
     console.log(response);
+  }
+
+
+  export function getFileWithHeader(target_url, tokenkey, tokenvalue, responseFunction) {
+
+    let myHeaders = new Headers();
+    myHeaders.append(tokenkey, tokenvalue);
+  
+    let requestOptions = {
+        method: 'GET',
+        redirect: 'follow',
+        headers: myHeaders
+    };
+  
+    fetch(target_url, requestOptions)
+        .then(response => {
+            if (response.status === 200) {
+                // Jika status 200, download file
+                return response.blob().then(blob => {
+                    const url = window.URL.createObjectURL(blob);
+                    const a = document.createElement('a');
+                    a.href = url;
+                    let base64fileurl = target_url.split('/').pop();
+                    a.download = atob(base64fileurl); // Nama file dari URL
+                    document.body.appendChild(a);
+                    a.click();
+                    a.remove();
+                });
+            } else {
+                // Jika status selain 200, parse sebagai JSON
+                return response.json().then(result => responseFunction(result));
+            }
+        })
+        .catch(error => console.log('error', error));
   }
